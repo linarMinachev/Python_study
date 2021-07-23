64d0962d0d5281c574324e62cce70ff7a05a8254
blindstudio <86982880+linarMinachev@users.noreply.github.com>
2021-07-23 17:14:07 +0300

    Lesson_4

--- task_5.py
+++ task_5.py
@@ -1,4 +1,22 @@
-# C:\Users\Blindstudio\Desktop\Python\Python_study>python task_4.py USD
-# 2021-07-24, 17.3264
+from sys import argv
+from requests import get, utils
+from datetime import date
 
-# Через терминал удалось вызвать без argv. Недоконца разобрался в нем
+response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+encodings = utils.get_encoding_from_headers(response.headers)
+content = response.content.decode(encoding=encodings)
+
+
+def currency_rates(kzt):
+    value = content.split("<Valute ID=")
+    d, m, y = map(int, value[0].split('"')[-4].split("."))
+
+    for i in value:
+        if kzt.upper() in i:
+            print(date(year=y, month=m, day=d), end=", ")
+            return float(i.replace("/", "").split("<Value>")[1].replace(",", "."))
+
+
+if __name__ == "__main__":
+    name = argv[1]
+    print(currency_rates(name))
